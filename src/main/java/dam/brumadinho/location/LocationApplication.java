3fa409f94df9755d0fb7cc2bd64e157037d646d0
Glauber Nespoli <glauber.nespoli@gmail.com>
2019-01-29 13:35:03 +0100

    web scrapping list and writing to csv file

--- src/main/java/dam/brumadinho/location/LocationApplication.java
+++ src/main/java/dam/brumadinho/location/LocationApplication.java
@@ -1,13 +1,35 @@
 package dam.brumadinho.location;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.opencsv.CSVWriter;
 
-@SpringBootApplication
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class LocationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocationApplication.class, args);
+	private static final String MISSING_PEOPLE_URL = "http://brumadinho.vale.com/listagem-pessoas-sem-contato.html";
+
+	private static final String CSV_PATH = "out/missing-people.csv";
+
+	public static void main(String[] args) throws IOException {
+		Path path = getPath();
+		try(CSVWriter writer = new CSVWriter(new FileWriter(path.toString()))) {
+			Jsoup.connect(MISSING_PEOPLE_URL)
+					.get()
+					.select("li")
+					.stream()
+					.map(Element::html)
+					.forEach(o -> writer.writeNext(new String[]{o}));
+		}
+	}
+
+	private static Path getPath() {
+		return Paths.get(CSV_PATH);
 	}
 
 }
